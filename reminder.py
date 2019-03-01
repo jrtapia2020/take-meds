3be95e35520193ed55e7466f819cebb43ff37e98
Jesus Tapia-Martinez <jesus.raul.tapia@gmail.com>
2019-03-01 00:35:29 -0700

    Added shebang and main method

--- reminder.py
+++ reminder.py
@@ -1,20 +1,26 @@
+#!/usr/bin/env python3
+
 import time
 import webbrowser
 
 # Simple repeating reminder to serve as framework for text reminder
 
 # TODO: Change to be able to call message.py rather than opening a web link
-# TODO: Implement a run.py command to run the function sending the text
 # TODO: implement cron jobs as a replacement for this loop
 
 # opens up song by Ariana Grande every day for a year
 
 
-def reminder() -> None:
-    print('This program ran on ' + time.ctime())
-    time.sleep(86400)
-    webbrowser.open('https://www.youtube.com/watch?v=LH4Y1ZUUx2g')
+def main() -> None:
+
+    def reminder() -> None:
+        print('This program ran on ' + time.ctime())
+        time.sleep(86400)
+        webbrowser.open('https://www.youtube.com/watch?v=LH4Y1ZUUx2g')
+
+    for i in range(365):
+        reminder()
 
 
-for i in range(365):
-    reminder()
+if __name__ == '__main__':
+    main()
