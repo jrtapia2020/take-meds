33c71dcebcc29c2aa5ac6be3a744b18098690806
Jesus Tapia-Martinez <jesus.raul.tapia@gmail.com>
2019-02-28 20:16:06 -0700

    Removed Twilio sid, token, and phone numbers for privacy.

--- message.py
+++ message.py
@@ -5,15 +5,15 @@ from twilio.rest import Client
 # TODO: Change to my wife's phone number
 
 # Your Account Sid and Auth Token from twilio.com/console
-account_sid = 'ACd96a309d2beb4203b877b8d31bb46950'
-auth_token = 'bcd91e23c082845f707ac282bcabee01'
+account_sid = ''    # Individual sid
+auth_token = ''     # Individual token
 client = Client(account_sid, auth_token)
 
 message = client.messages \
                 .create(
                      body="This test message works!",
-                     from_='+12015716417',
-                     to='+16026421037'
+                     from_='',  # Twilio phone number
+                     to=''      # Desired destination
                  )
 
 print(message.sid)
